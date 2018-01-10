199c9c25660469ddd2b21321a4fcebd88104be87
Kayla Ondracek and Stephanie Faber <stephanie@>
2018-01-10 09:11:27 -0800

    Change story text and the corresponding form and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
+    var vehicleInput= $("input#vehicle").val();
+    var numberInput = $("input#number").val();
     var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var professionInput = $("input#profession").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
+    $(".vehicle").text(vehicleInput);
+    $(".number").text(numberInput);
     $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".profession").text(professionInput);
 
     $("#story").show();
 
